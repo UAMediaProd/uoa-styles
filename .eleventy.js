38d80b71398c4758feb8f61aefba6fdecf22cd92
Tim Klapdor <tim.klapdor@gmail.com>
2021-02-07 01:00:50 +1030

    Building out components

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,24 @@
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/css");
+
+  // Markdown
+  const markdownOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true,
+  };
+
+  const MarkdownIt = require("markdown-it");
+  const mdRender = new MarkdownIt();
+  eleventyConfig.addFilter("renderUsingMarkdown", function(rawString) {
+  return mdRender.render(rawString);
+  });
+
 
   return {
     dir: {
