f0a53e6c44af664e5e5f5c2e54aaa4dbca1b66d0
Aaron Honson <honsyb@gmail.com>
2023-02-09 10:29:42 +1030

    adx.js - test running API calls from central location

--- public/assets/adx.js
+++ public/assets/adx.js
@@ -1,3 +1,11 @@
-function adxFunc(ye){
-    console.log(ye);
+function adxFunc(courseID){
+    console.log("adxFunc called");
+    fetch('/api/v1/courses/' + courseID +'/modules?access_token=7036~bVNFBUILJmAN3kmNgHtFvU1VE36pMclqXX6EYOq8gcegREOEtwbWZdGBNjjhLhyk', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then((response) => {
+        response.json().then((json) => console.log(json));
+    });
 }
