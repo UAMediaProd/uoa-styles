552c5cb7eb98f76ccf9bbfc80251460087bf90bf
Aaron Honson <honsyb@gmail.com>
2023-02-09 13:48:06 +1030

    make adxFunc then-able

--- public/assets/adx.js
+++ public/assets/adx.js
@@ -1,6 +1,6 @@
-function adxFunc(courseID){
+async function adxFunc(courseID){
     console.log("adxFunc called");
-    fetch('/api/v1/courses/' + courseID +'/modules?access_token=7036~bVNFBUILJmAN3kmNgHtFvU1VE36pMclqXX6EYOq8gcegREOEtwbWZdGBNjjhLhyk', {
+    return fetch('/api/v1/courses/' + courseID +'/modules?access_token=7036~bVNFBUILJmAN3kmNgHtFvU1VE36pMclqXX6EYOq8gcegREOEtwbWZdGBNjjhLhyk', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
