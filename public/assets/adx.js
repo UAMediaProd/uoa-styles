9b76b8e1032d07d76a9a104c2ef8303d456732fa
Aaron Honson <honsyb@gmail.com>
2023-02-09 13:56:07 +1030

    JS Class experiments

--- public/assets/adx.js
+++ public/assets/adx.js
@@ -1,11 +1,26 @@
-async function adxFunc(courseID){
-    console.log("adxFunc called");
-    return fetch('/api/v1/courses/' + courseID +'/modules?access_token=7036~bVNFBUILJmAN3kmNgHtFvU1VE36pMclqXX6EYOq8gcegREOEtwbWZdGBNjjhLhyk', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).then((response) => {
-       return response.json();
-    });
+//ADX API Class
+
+class adxHelper {
+    constructor(courseID){
+        this.courseID = courseID;
+    }
+    async getModules(){
+        return fetch('/api/v1/courses/' + this.courseID +'/modules?access_token=7036~bVNFBUILJmAN3kmNgHtFvU1VE36pMclqXX6EYOq8gcegREOEtwbWZdGBNjjhLhyk', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then((response) => {
+            return response.json();
+        });
+    }
 }
+
+
+
+
+
+
+
+
+
